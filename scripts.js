91f43c6cc875032033c2703aa1f267396f492170
Incognian <hardcorpsbusiness12345@gmail.com>
2026-05-25 14:22:00 +0700

    ghiluiuyhyihi

--- scripts.js
+++ scripts.js
@@ -1,15 +1,84 @@
+const con = "http://localhost:3000/";
+
+async function redirect()
+{
+    const email = localStorage.getItem('email');
+    const password = localStorage.getItem('password');
+
+    const response = await fetch(con + "login/",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: email,
+            pw: password
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+
+    path = window.location.pathname;
+    const htmlName = path.substring(path.lastIndexOf('/') + 1);
+    if (data.resID != null)
+    {
+        if (htmlName == 'index.html') window.location.href = "schedule.html";
+    }
+    else if (htmlName != 'index.html') window.location.href = "index.html";
+}
+
 async function register()
 {
     const name = document.getElementById("reg-name").value;
     const email = document.getElementById("reg-email").value;
     const password = document.getElementById("reg-pass").value;
 
-    const response = await fetch("http://localhost:3000/register/" + name + "/" + email + "/" + password,
+    const response = await fetch(con + "register",
     {
-        method: 'POST'
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            name: name,
+            email: email,
+            pw: password
+        })
     });
     const data = await response.json();
-
-    console.log("test");
     console.log(data);
+}
+
+async function login()
+{
+    const email = document.getElementById("log-email").value;
+    const password = document.getElementById("log-pass").value;
+
+    const response = await fetch(con + "login",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: email,
+            pw: password
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+
+    if (data.resID != null)
+    {
+        localStorage.setItem('email', email);
+        localStorage.setItem('password', password);
+        redirect();
+    }
 }
