5e2e60b75613cecc3de6425bb2604abb5d42a8f1
Incognian <hardcorpsbusiness12345@gmail.com>
2026-06-07 12:46:02 +0700

    FrontEnd Done!

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ async function redirect()
     // index.html becomes login page
     if (data.resID != null)
     {
-        // if already logged in but still in index.html go to main page
+        // if already logged in but still in index.html go to main page (schedule)
         if (htmlName == 'index.html' || htmlName == '')
         {
             openPage('schedule');
@@ -34,6 +34,30 @@ async function redirect()
     }
     // if not on index.html but not logged in go to index.html
     else if (htmlName != 'index.html' && htmlName != '') openPage("index");
+}
+
+async function kick(allowed)
+{
+    const email = localStorage.getItem('email');
+    const password = localStorage.getItem('password');
+
+    const response = await fetch(con + "getID",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: email,
+            pw: password
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+
+    if (!allowed[data.resTipe - 1]) openPage('schedule');
 }
 
 async function register()
@@ -54,6 +78,37 @@ async function register()
             name: name,
             email: email,
             pw: password
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+
+    alert(data.success ? "registration success" : "registration failed");
+    // register doesn't automatically log into newly registered account
+}
+
+async function registerAdmin()
+{
+    const name = document.getElementById("reg-name").value;
+    const regemail = document.getElementById("reg-email").value;
+    const regpw = document.getElementById("reg-pass").value;
+    const tipe = document.getElementById("reg-tipe").value;
+
+    const response = await fetch(con + "registerAdmin",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: localStorage.getItem("email"),
+            pw: localStorage.getItem("password"),
+            name: name,
+            regemail: regemail,
+            regpw: regpw,
+            tipe: tipe
         })
     });
     const data = await response.json();
@@ -117,20 +172,21 @@ async function showSchedule()
             email: localStorage.getItem('email'),
             pw: localStorage.getItem('password'),
             // ignore inputID for now
-            inputID: 0
+            inputID: document.getElementById("user-selector-dropdown").value
         })
     });
     const data = await response.json();
     console.log(data);
 
-    data.forEach(e => putBox(e.hari, e.jamAwal, e.jamAkhir, e.nama, e.id, e.mapel, e.jenjang));
+    data.forEach(e => putBox(e.idJadwal, e.idKursus, e.hari, e.jamAwal, e.jamAkhir,
+        e.nama, e.id, e.mapel, e.jenjang));
 }
 
-function putBox(hari, jamAwal, jamAkhir, nama, id, mapel, jenjang)
+function putBox(idJadwal, idKursus, hari, jamAwal, jamAkhir, nama, id, mapel, jenjang)
 {
     const box = document.createElement("div");
     box.className = "schedule-box";
-    box.innerHTML = `${mapel}<br>${jenjang}<br>${nama} (${id})`;
+    box.innerHTML = `(${idJadwal}, ${idKursus})<br>${mapel} ${jenjang}<br>${nama} (${id})`;
     let col = 1;
     if (hari == 'Selasa') col = 2;
     else if (hari == 'Rabu') col = 3;
@@ -141,6 +197,7 @@ function putBox(hari, jamAwal, jamAkhir, nama, id, mapel, jenjang)
     
     box.style.gridColumn = `${col} / ${col + 1}`;
     box.style.gridRow = `${jamAwal - 7} / ${jamAkhir - 7}`;
+    box.style.maxHeight = `${10 * (jamAkhir - jamAwal)}rem`;
     document.getElementById("actual-schedule").appendChild(box);
 }
 
@@ -170,19 +227,12 @@ async function hideObjects()
     const data = await response.json();
     console.log(data);
 
-    document.querySelectorAll('[viewableby]').forEach(e =>
-    {
-        if (e.getAttribute("viewableby") != data.resTipe) e.style.display = "none";
-    });
+    if (data != null && data.resTipe != 3)
+        document.querySelectorAll('[viewableby]').forEach(e =>
+        {
+            if (e.getAttribute("viewableby") != data.resTipe) e.style.display = "none";
+        });
 }
-
-// async function fillRecordTable(index, tableID, columns)
-// {
-//     const response = await fetch()
-//     {
-
-//     }
-// }
 
 async function populateDropdown(index, dropdownID)
 {
@@ -210,6 +260,7 @@ async function populateDropdown(index, dropdownID)
 
         target.appendChild(item);
     });
+    target.value = null;
 }
 
 async function populateGuruDropdown()
@@ -240,8 +291,7 @@ async function populateGuruDropdown()
         item.textContent = `${e.idPengguna} - ${e.namaPengguna}`;
         target.appendChild(item);
     });
-
-    loadGuruSchedule();
+    target.value = null;
 }
 
 async function loadGuruSchedule()
@@ -329,7 +379,7 @@ async function deleteGuruSchedule(inputID)
 }
 
 async function changeAccountInformation() {
-    // 1. Grab all input values
+    // 1. Grab all input textContents
     const email1 = document.getElementById("email-kontak-1").value.trim();
     const email2 = document.getElementById("email-kontak-2").value.trim();
     const phone1 = document.getElementById("nomor-kontak-1").value.trim();
@@ -400,26 +450,72 @@ async function loadAccountInformation()
     console.log(data);
 
     if (data.resEmail.length > 0)
-        document.getElementById("email-kontak-1").value =
+        document.getElementById("email-kontak-1").textContent =
         data.resEmail[0].alamatEmail;
     if (data.resEmail.length > 1)
-        document.getElementById("email-kontak-2").value =
+        document.getElementById("email-kontak-2").textContent =
         data.resEmail[1].alamatEmail;
     if (data.resPhone.length > 0)
-        document.getElementById("nomor-kontak-1").value =
+        document.getElementById("nomor-kontak-1").textContent =
         data.resPhone[0].nomorTelepon;
     if (data.resPhone.length > 1)
-        document.getElementById("nomor-kontak-2").value =
+        document.getElementById("nomor-kontak-2").textContent =
         data.resPhone[1].nomorTelepon;
     if (data.resPengalaman.length > 0)
-        document.getElementById("pengalaman-1").value =
+        document.getElementById("pengalaman-1").textContent =
         data.resPengalaman[0].deskripsi;
     if (data.resPengalaman.length > 1)
-        document.getElementById("pengalaman-2").value =
+        document.getElementById("pengalaman-2").textContent =
         data.resPengalaman[1].deskripsi;
     if (data.resJenjang != null)
-        document.getElementById("idJenjang").value =
+        document.getElementById("idJenjang").textContent =
         data.resJenjang
+}
+
+async function searchAccount()
+{
+    const response = await fetch(con + "searchUser",
+    {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            inputID: document.getElementById("search-ID").value
+        })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if (data.resEmail.length > 0)
+        document.getElementById("email-kontak-1-res").textContent =
+        `${data.resEmail[0].alamatEmail}`;
+    else document.getElementById("email-kontak-1-res").textContent = "-";
+    if (data.resEmail.length > 1)
+        document.getElementById("email-kontak-2-res").textContent =
+        `${data.resEmail[1].alamatEmail}`;
+    else document.getElementById("email-kontak-2-res").textContent = "-";
+    if (data.resPhone.length > 0)
+        document.getElementById("nomor-kontak-1-res").textContent =
+        `${data.resPhone[0].nomorTelepon}`;
+    else document.getElementById("nomor-kontak-1-res").textContent = "-";
+    if (data.resPhone.length > 1)
+        document.getElementById("nomor-kontak-2-res").textContent =
+        `${data.resPhone[1].nomorTelepon}`;
+    else document.getElementById("nomor-kontak-2-res").textContent = "-";
+    if (data.resPengalaman.length > 0)
+        document.getElementById("pengalaman-1-res").textContent =
+        `${data.resPengalaman[0].deskripsi}`;
+    else document.getElementById("pengalaman-1-res").textContent = "-";
+    if (data.resPengalaman.length > 1)
+        document.getElementById("pengalaman-2-res").textContent =
+        `${data.resPengalaman[1].deskripsi}`;
+    else document.getElementById("pengalaman-2-res").textContent = "-";
+    if (data.resJenjang != null)
+        document.getElementById("idJenjang-res").textContent =
+        `${data.resJenjang}`;
+    else document.getElementById("idJenjang-res").textContent = "-";
 }
 
 async function loadWillingness()
@@ -493,7 +589,7 @@ async function deleteWillingness(idMapel, idJenjang)
         ({
             email: localStorage.getItem('email'),
             pw: localStorage.getItem('password'),
-            inputID: null,
+            inputID: document.getElementById("user-selector-dropdown").value,
             idMapel: idMapel,
             idJenjang: idJenjang
         })
@@ -502,6 +598,203 @@ async function deleteWillingness(idMapel, idJenjang)
     if (data.success) loadWillingness();
 }
 
-// on page startup call redirect
+async function loadSlots()
+{
+    const tbody = document.getElementById("schedule-table");
+    tbody.innerHTML = "";
+    console.log("loading slots");
+
+    // Determine which guru to load slots for
+    const guruID = document.getElementById("user-selector-dropdown").value;
+
+    const response = await fetch(con + "getSlots",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: localStorage.getItem('email'),
+            pw: localStorage.getItem('password'),
+            inputID: guruID
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+
+    data.forEach(e =>
+    {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td>${e.hari ?? ""}</td>
+                         <td>${e.jamAwal ?? ""}</td>
+                         <td>${e.jamAkhir ?? ""}</td>
+                         <td classname='action-icon' onclick="deleteSlot('${e.hari}', ${e.jamAwal}, ${e.jamAkhir})">✕</td>`;
+        tbody.appendChild(row);
+    });
+}
+
+async function addSlot()
+{
+    const guruID = document.getElementById("user-selector-dropdown").value;
+    
+    const response = await fetch(con + "addSlot",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: localStorage.getItem('email'),
+            pw: localStorage.getItem('password'),
+            param:
+            [
+                guruID,
+                document.getElementById("dayVal").value,
+                document.getElementById("startTime").value,
+                document.getElementById("endTime").value
+            ]
+        })
+    });
+    const data = await response.json();
+
+    if (!data.success) alert("addition failed");
+    else loadSlots();
+}
+
+async function deleteSlot(hari, jamAwal, jamAkhir)
+{
+    const response = await fetch(con + "deleteSlot",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: localStorage.getItem('email'),
+            pw: localStorage.getItem('password'),
+            idGuru: document.getElementById("user-selector-dropdown").value,
+            hari: hari,
+            jamAwal: jamAwal,
+            jamAkhir: jamAkhir
+        })
+    });
+    const data = await response.json();
+    if (data.success) loadSlots();
+}
+
+let selectedTimeSlots = [];
+
+function selectRow(row)
+{
+    selectedTimeSlots.push(row);
+    
+    const cell = row.children[5];
+    cell.setAttribute("onclick", "deselectRow(this.parentElement)");
+    cell.textContent = "✕";
+    
+    row.remove();
+    document.getElementById("selected-time-slots").appendChild(row);
+}
+
+function deselectRow(row)
+{
+    selectedTimeSlots.filter(item => item !== row);
+    
+    const cell = row.children[5];
+    cell.setAttribute("onclick", "selectRow(this.parentElement)");
+    cell.textContent = "✛";
+    
+    row.remove();
+    document.getElementById("unselected-time-slots").appendChild(row);
+}
+
+async function showValidSlots()
+{
+    selectedTimeSlots = [];
+    const response = await fetch(con + "iHateMyself",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            mapel: document.getElementById('mapel').value,
+            jenjang: document.getElementById('jenjang').value
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+
+    const tbody = document.getElementById("unselected-time-slots");
+    tbody.innerHTML = "";
+    document.getElementById("selected-time-slots").innerHTML = "";
+
+    data.forEach(e =>
+    {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td>${e.idGuru ?? ""}</td>
+                        <td>${e.namaGuru ?? ""}</td>
+                        <td>${e.hari ?? ""}</td>
+                        <td>${e.jamAwal ?? ""}</td>
+                        <td>${e.jamAkhir ?? ""}</td>
+                        <td class='action-icon' onclick="selectRow(this.parentElement)">✛</td>`;
+        row.value =
+        {
+            idGuru: e.idGuru,
+            hari: e.hari,
+            jamAwal: e.jamAwal,
+            jamAkhir: e.jamAkhir
+        }
+        tbody.appendChild(row);
+    });
+}
+
+async function createCourse()
+{
+    let rows = [];
+    for (const e of selectedTimeSlots)
+    {
+        rows.push(e.value);
+    }
+    selectedTimeSlots = [];
+    console.log(rows);
+
+    const response = await fetch(con + "hardestPartHere",
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify
+        ({
+            email: localStorage.getItem('email'),
+            pw: localStorage.getItem('password'),
+            mapel: document.getElementById('mapel').value,
+            jenjang: document.getElementById('jenjang').value,
+            tanggalAwal: document.getElementById('tanggalAwal').value,
+            tanggalAkhir: document.getElementById('tanggalAkhir').value,
+            rows: rows
+        })
+    });
+    const data = await response.json();
+    
+    if (!data.success) alert("course creation failed!");
+    else
+    {
+        alert("course creation success!");
+        openPage("schedule");
+    }
+}
+
+// on page startup call redirect and hideObjects
 document.addEventListener("DOMContentLoaded", redirect);
 document.addEventListener("DOMContentLoaded", hideObjects);
